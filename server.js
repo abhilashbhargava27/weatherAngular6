b5fc3512ef342889e01eb32cb7d70f97ed71f42e
Abhilash <abhilash@Abhilashs-MacBook-Pro.local>
2018-06-23 16:00:58 +0530

    attempt to deploy on heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/weatherReport'));
+app.use(express.static(__dirname + '/dist/weatherAngular6'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/weatherReport/index.html'));
+res.sendFile(path.join(__dirname+'/dist/weatherAngular6/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
